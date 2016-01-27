7de593ba82214766c34d7949ea1174bc45918acc
Yael <yaelgogol@gmail.com>
2016-01-27 22:46:36 +0200

    f6

--- Question/urls.py
+++ Question/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('questions.urls')),
     url(r'^questions/', include('questions.urls')),
     url(r'^admin/', admin.site.urls),
 ]
