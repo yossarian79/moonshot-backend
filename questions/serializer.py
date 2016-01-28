d33d229829f7ba65e5fc984706261c1c853a26a5
Yael <yaelgogol@gmail.com>
2016-01-28 19:11:25 +0200

    first4

--- questions/serializer.py
+++ questions/serializer.py
@@ -4,4 +4,4 @@ from .models import Question
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Question
-        fields = ('id','author','title','description')
+        fields = ('id','title','description')
